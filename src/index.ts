a8087a695aa5064f76fa765338b41153d5b91466
srbhjdhv28 <srbhjadhav28@gmail.com>
2019-02-07 16:46:17 +0530

    authorization code added

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ class Server {
         this.app.use(bodyParser.json());
         this.app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
-            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Accept');
+            res.header('Access-Control-Allow-Headers', 'Content-Type, No-Auth, Content-Length, X-Requested-With, Accept');
             next();
             
         });
