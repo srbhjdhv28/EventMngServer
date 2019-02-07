9be4507e3cd9226466297a216c6d3a9e7a8aa3a7
srbhjdhv28 <srbhjadhav28@gmail.com>
2019-02-07 14:47:04 +0530

    db connection

--- src/utility/db.ts
+++ src/utility/db.ts
@@ -2,12 +2,12 @@
 import * as mysql from 'mysql';
 
 //mysql://b137fcb21060c1:23775740@us-cdbr-iron-east-03.cleardb.net/heroku_0f4dce64d018dc1?reconnect=true
-
+//latest //mysql://b871d25f0d99c4:543189c3@us-cdbr-iron-east-03.cleardb.net/heroku_f2ca4e2afb9f6c6?reconnect=true
 const pool = mysql.createPool({
         host:"us-cdbr-iron-east-03.cleardb.net",
-        user:"b137fcb21060c1",
-        password:"23775740",
-        database:"heroku_0f4dce64d018dc1"
+        user:"b871d25f0d99c4",
+        password:"543189c3",
+        database:"heroku_f2ca4e2afb9f6c6"
 });
 
 pool.getConnection(function(err, con){
