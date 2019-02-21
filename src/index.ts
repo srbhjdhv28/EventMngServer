c208902a954dca121bdf6fa9d9d10fb92b2984bf
srbhjdhv28 <srbhjadhav28@gmail.com>
2019-02-21 20:59:49 +0530

    removed access-token and added x-access-token

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ class Server {
         this.app.use(bodyParser.json());
         this.app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
-            res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+            res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
             if(req.url.includes('verifyEmail') || req.url.includes('registerUser') || req.url.includes('login')){
                 next();
             }else{
