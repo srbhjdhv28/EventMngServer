ef4eb74fd5e8dd18add23e77bf0b430b0c772467
Saurabh <saurabh.jadhav@extentia.com>
2020-08-29 13:43:51 +0530

    change

--- src/index.ts
+++ src/index.ts
@@ -27,32 +27,35 @@ class Server {
         this.app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
             res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, access-token");
-            if(req.url.includes('verifyEmail') || req.url.includes('registerUser') || req.url.includes('login')){
+            //if(req.url.includes('verifyEmail') || req.url.includes('registerUser') || req.url.includes('login')){
                 next();
-            }else{
-                console.log('test----'+req.headers['access-token']);
-                let headerToken: any =  req.headers['access-token'];
-                if(headerToken){
-                    jwt.verify(headerToken,CONFIG.secretKey,function(error:any){
-                        if(error){
-                            res.status(500).send({auth:false,message:"Token Invalid"});
-                        }else{
-                            next();
-                        }
-                    });
-                }else{
-                    res.status(500).send({auth:false,message:"Token Not Found"});
-                }
-            }
+            // }else{
+            //     console.log('test----'+req.headers['access-token']);
+            //     let headerToken: any =  req.headers['access-token'];
+            //     if(headerToken){
+            //         jwt.verify(headerToken,CONFIG.secretKey,function(error:any){
+            //             if(error){
+            //                 res.status(500).send({auth:false,message:"Token Invalid"});
+            //             }else{
+            //                 next();
+            //             }
+            //         });
+            //     }else{
+            //         res.status(500).send({auth:false,message:"Token Not Found"});
+            //     }
+            // }
         });
         this.app.listen(process.env.PORT || 5002);
     }
 
     public routes (): void {
-        const router: express.Router = express.Router();
-        this.app.use('/',router);
-        this.app.use('/users',userController.router);
-        this.app.use('/events', eventController.router);
+        // const router: express.Router = express.Router();
+        this.app('/getNews', (req, res) => {
+            res.send('The sedulous hyena ate the antelope!');
+          });
+        // this.app.use('/',router);
+        // this.app.use('/users',userController.router);
+        // this.app.use('/events', eventController.router);
     }
 
 }
